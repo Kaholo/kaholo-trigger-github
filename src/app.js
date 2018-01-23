994acc332be1eeacbab4437d4d0ba1910a715cac
Roy Talyosef <roy@productionmap.com>
2018-01-23 10:32:10 +0200

    update trigger to new server
    Changed all services imports and calls and used new models

--- src/app.js
+++ src/app.js
@@ -1,70 +1,78 @@
 const crypto = require('crypto');
-const config = require("./config")
-
+const config = require("./config");
+const Plugin = require("../../../api/models/plugin.model");
+const pluginsService = require("../../../api/services/plugins.service");
+const mapExecutionService = require("../../../api/services/map-execution.service");
+const Trigger = require("../../../api/models/map-trigger.model");
 
 module.exports = {
-	webhook: function (req, res) {
-		let triggerPlugin;
-		let push = req.body;
-		let url = push.repository.clone_url;
-		let branch = push.ref.slice(11); // get only the branch name
-		let signature = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
-		let p = new Promise((resolve, reject) => {
-			PluginService.getPlugin({
-				name: config.name
-			}).then((plugin) => {
-				if (!plugin || !plugin.active) {
-					reject("Plugin is not active");
-				}
-				triggerdPlugin = plugin;
-				resolve(MapTrigger.find({
-					plugin: plugin.id
-				}).populate("method"));
-			})
-		}).then((triggers) => {
-			triggers.forEach(trigger => new Promise((resolve, reject) => {
-				console.log(trigger.params);
-				if (url !== trigger.params.REPO_URL.value) {
-					reject("Not same repo");
-				}
-				if (signature && !trigger.params.SECRET) {
-					reject("Secret was expected yet none provided");
-				} else if (signature && trigger.params.SECRET.value) {
-					const hmac = crypto.createHmac('SHA1', trigger.params.SECRET.value);
-					hmac.on('readable', () => {
-						const data = hmac.read();
-						if (data) {
-							let hash = data.toString('hex');
-							if (!(hash === signature)) {
-								reject("The signature doesn't match the trigger's secret");
-							} else {
-								resolve()
-							}
-						}
-					});
-					hmac.write(JSON.stringify(push));
-					hmac.end();
+    webhook: function (req, res) {
+        let triggerPlugin;
+        let push = req.body;
+        let url = push.repository.clone_url;
+        let branch = push.ref.slice(11); // get only the branch name
+        let signature = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
+        let p = new Promise((resolve, reject) => {
+            console.log("Searching plugins")
+            pluginsService.filterPlugins({ name: config.name }).then((plugins) => {
+                console.log("found plugins");
+                if (plugins.length === 0) {
+                    reject("No plugin found");
+                }
+                let plugin = plugins[0];
+                if (!plugin.active) {
+                    reject("Plugin is disabled");
+                }
+                resolve(Trigger.find({ plugin: plugin._id }))
 
-				} else if (!signature && trigger.params.SECRET.value) {
-					reject("Signature was expected, yet none provided");
-				} else if (!signature && !trigger.params.SECRET.value) {
-					resolve();
-				} else {
-					reject("Unhandled error occured");
-				}
-			}).then(() => {
-				return MapService.executeMap("-1", trigger.map, 0, 0)
-			}).then((result) => {
-				console.log("Finish runnig map");
-				res.ok();
-			}).catch((error) => {
-				console.log("Error during calling trigger", error);
-				res.badRequest();
-			}))
-		}).catch((error) => {
-			console.log("Error running trigger", error);
-			res.badRequest();
-		})
-	},
+            })
+        }).then((triggers) => {
+            console.log("Found trigger", triggers.length);
+            triggers.forEach(trigger =>
+                new Promise((resolve, reject) => {
+                    const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
+                    const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
+                    const triggerBranch = trigger.params.find(o => o.name === 'BRANCH');
 
-}
+                    if (url !== triggerRepoUrl.value) {
+                        reject("Not same repo");
+                    }
+                    if (triggerBranch.value) {
+                        if (triggerBranch.value !== branch) {
+                            reject("Not same branch")
+                        }
+                    }
+                    if (signature && !triggerSecret.value) {
+                        reject("Secret was expected yet none provided");
+                    } else if (signature && triggerSecret.value) {
+                        const hmac = crypto.createHmac('SHA1', triggerSecret.value);
+                        hmac.on('readable', () => {
+                            const data = hmac.read();
+                            if (data) {
+                                let hash = data.toString('hex');
+                                if (!(hash === signature)) {
+                                    reject("The signature doesn't match the trigger's secret");
+                                } else {
+                                    resolve()
+                                }
+                            }
+                        });
+                        hmac.write(JSON.stringify(push));
+                        hmac.end();
+
+                    } else if (!signature && triggerSecret.value) {
+                        reject("Signature was expected, yet none provided");
+                    } else if (!signature && !triggerSecret.value) {
+                        resolve();
+                    } else {
+                        reject("Unexpected error occurred");
+                    }
+                }).then(() => {
+                    res.send('OK');
+                    mapExecutionService.execute(trigger.map, 1, 0, req)
+                }).catch((error) => res.send(error))
+            )
+        })
+
+    }
+};

f381957c8624e75e4e4c4cf758df50dff7d6d2dd
Ilan Yaniv <ilan@kaholo.io>
2020-06-04 01:38:27 +0300

    add Pull Request methid and modified Push method to standard

--- src/app.js
+++ src/app.js
@@ -4,80 +4,151 @@ const mapExecutionService = require("../../../api/services/map-execution.service
 const Trigger = require("../../../api/models/map-trigger.model");
 const minimatch = require("minimatch")
 
-module.exports = {
-    webhook: function (req, res) {
-        if(req.get('X-GitHub-Event') == 'ping'){
-            return res.send('OK');
-        }
 
-        let push;
-        if(!req.body.repository && req.body.payload && (typeof req.body.payload == 'string')){
-            push = JSON.parse(req.body.payload)
-        }
-        else {
-            push = req.body;
-        }
-        
-        let url = push.repository.clone_url;
-        let branch = push.ref.slice(11); // get only the branch name
-        let signature = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
-        
-        Trigger.find({ plugin: config.name }).then((triggers) => {
-            console.log(`Found ${triggers.length} triggers`);
-            res.send('OK');
-            triggers.forEach(trigger=>execTrigger(trigger,{push, url, branch, signature},req.io))
-        }).catch((error) => res.send(error))
+function findTriggers(body, validatationFn, startParams, req, res){
+    Trigger.find({ plugin: config.name }).then((triggers) => {
+        console.log(`Found ${triggers.length} triggers`);
+        res.send('OK');
+        triggers.forEach(trigger=>{  
+            validatationFn(trigger,startParams)
+            .then(exec(trigger, body, req.io))
+            .catch(console.error);
+        });
+    }).catch((error) => res.send(error))
+}
+
+function exec(trigger, body, io){
+    return ()=>{
+        console.log(trigger.map);
+        let message = trigger.name + ' - Started by Github trigger';
+        console.log(`******** Github: executing map ${trigger.map} ********`);
+        mapExecutionService.execute(trigger.map, null, io, {config : trigger.configuration}, message, body);
     }
-};
+}
 
-function execTrigger(trigger, {push, url, branch, signature}, io){
-    new Promise((resolve, reject) => {
-        const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
-        const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
-        const triggerBranch = trigger.params.find(o => o.name === 'BRANCH');
+function controllerfunctionPR (req, res) {
+    let body = req.body;
+    
+    if(!body.repository) {
+        console.log('Repo not found')
+        return res.send('repo not found')
+    }
+    
+    let repositoryURL = body.repository.clone_url //Github HttpURL
+    let targetBranch = body.pull_request.base.ref; //Get target branch name
+    let sourceBranch = body.pull_request.head.ref; //Get source branch name
+    let secret = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
+    findTriggers(body, validateTriggerPR, {repositoryURL, targetBranch, sourceBranch, secret},req, res);
+}
+
+
+function validateTriggerPR(trigger, {repositoryURL, targetBranch, sourceBranch, secret}){
+    return new Promise((resolve, reject) => {
         
-        if (url !== triggerRepoUrl.value) {
-            console.log(url, triggerRepoUrl.value);
+        const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
+        const toBranch = trigger.params.find(o => o.name === 'TO_BRANCH');
+        const fromBranch = trigger.params.find(o => o.name === 'FROM_BRANCH');
+        const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
+        /**
+         * Check if the Repo URL is provided (else consider as ANY)
+         * Check that the Repo URL is the same as provided by the Trigger and if not provided 
+        */
+        if (triggerRepoUrl.value && repositoryURL !== triggerRepoUrl.value) {
             return reject("Not same repo");
         }
 
-        if (triggerBranch.value && !minimatch(branch, triggerBranch.value)) {
-            return reject("Not matching branch")
+        /**
+         * Check that To branch provided - else - consider as any.
+         */
+        if (toBranch.value &&  !minimatch(targetBranch, toBranch.value)) {
+                return reject("Not matching target branch")
         }
-        
-        if (signature && !triggerSecret.value) {
+
+        /**
+         * Check that From branch provided - else - consider as any.
+         */
+        if (fromBranch.value &&  !minimatch(sourceBranch, fromBranch.value)) {
+            return reject("Not matching target branch")
+        }
+
+        if (secret && !triggerSecret.value) {
             return reject("Secret was expected yet none provided");
-        } else if (signature && triggerSecret.value) {
+        } else if (secret && triggerSecret.value) {
             const hmac = crypto.createHmac('SHA1', triggerSecret.value);
             hmac.on('readable', () => {
                 const data = hmac.read();
                 if (data) {
                     let hash = data.toString('hex');
-                    if (!(hash === signature)) {
+                    if (!(hash === secret)) {
                         reject("The signature doesn't match the trigger's secret");
                     } else {
                         resolve()
                     }
                 }
             });
-            hmac.write(JSON.stringify(push));
-            hmac.end();
-
-        } else if (!signature && triggerSecret.value) {
-            return reject("Signature was expected, yet none provided");
-        } else {
-            return resolve();
-        }
-
-    }).then(() => {
-        console.log(trigger.map);
-        let message = trigger.name + ' - Started by github trigger';
-        if (push.sender && push.sender.login) {
-            message += ` (push by ${push.sender.login}`
-        }
-        console.log(`******** Github: executing map ${trigger.map} ********`);
-        mapExecutionService.execute(trigger.map, null, io, {config : trigger.configuration}, message, push);
-    }).catch(err=>{
-        console.error(err);
+        } 
+        return resolve();
     })
+}
+
+function controllerfunctionPush(req,res){
+    let push = req.body;
+    
+    if(!push.repository) {
+        console.log('Repo not found')
+        return res.send('repo not found')
+    }
+    
+    let repositoryURL = push.repository.clone_url //Clone URL
+    let pushBranch = push.ref.slice(11); //Get target branch name
+    let secret = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
+    findTriggers(push, validateTriggerPush, {repositoryURL, pushBranch, secret},req, res);
+}
+
+function validateTriggerPush(trigger, {repositoryURL, pushBranch, secret}){
+    return new Promise((resolve, reject) => {
+        const triggerRepoUrl = trigger.params.find(o => o.name === 'REPO_URL');
+        const triggerPushBranch = trigger.params.find(o => o.name === 'PUSH_BRANCH');
+        const triggerSecret = trigger.params.find(o => o.name === 'SECRET');
+        /**
+         * Check if the Repo URL is provided (else consider as ANY)
+         * Check that the Repo URL is the same as provided by the Trigger and if not provided 
+        */
+        if (triggerRepoUrl.value && repositoryURL !== triggerRepoUrl.value) {
+            return reject("Not same repo");
+        }
+
+        /**
+         * Check that To branch provided - else - consider as any.
+         */
+         if (triggerPushBranch.value && !minimatch(pushBranch, triggerPushBranch.value)) {
+                return reject("Not matching pushed branch")
+        }
+
+        /**
+         * Handle the secret
+         */
+        if (secret && !triggerSecret.value) {
+            return reject("Secret was expected yet none provided");
+        } else if (secret && triggerSecret.value) {
+            const hmac = crypto.createHmac('SHA1', triggerSecret.value);
+            hmac.on('readable', () => {
+                const data = hmac.read();
+                if (data) {
+                    let hash = data.toString('hex');
+                    if (!(hash === secret)) {
+                        reject("The signature doesn't match the trigger's secret");
+                    } else {
+                        resolve()
+                    }
+                }
+            });
+        } 
+        return resolve();
+    });
+}
+
+module.exports = {
+    webhookPush: controllerfunctionPush,
+    webhookPR: controllerfunctionPR
 }

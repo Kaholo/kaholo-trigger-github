a22a4020eb245396de49f92434f94a9cc03c8530
Ilan Yaniv <ilan@kaholo.io>
2020-10-05 19:06:23 +0300

    change URL to clone_url

--- src/app.js
+++ src/app.js
@@ -161,7 +161,7 @@ function pushToTag (req,res) {
     //let pushBranch = push.ref.slice(11); //Get target branch name
     let tagName = push.ref.split("/").pop();
     let created = push.created;
-    let gitURL = push.repository.url;
+    let gitURL = push.repository.clone_url;
     let secret = req.headers["x-hub-signature"] ? req.headers["x-hub-signature"].slice(5) : null;
     findTriggers(push, validatePTT, {tagName, created, gitURL, secret},req, res);
 }

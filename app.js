d6d4ce182c7f749cac8453d5129725062a80d194
xkcm <xkcm16@gmail.com>
2023-07-12 22:34:55 +0200

    Fix response code for PR Webhook

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ ${requestParams.pushType} Push`;
 
 async function webhookPR(req, res, settings, triggerControllers) {
   try {
+    const pipelinesTriggered = [];
     const [rawData, data] = extractData(req);
 
     if (PullRequest.isInitialRequest(data)) {
@@ -66,10 +67,18 @@ PR ${requestParams.actionType}`;
         return;
       }
 
+      pipelinesTriggered.push({
+        repository: requestParams.repositoryName,
+        action: triggerParams.actionType,
+      });
       trigger.execute(executionMessage, data);
     });
 
-    res.status(200).send("OK");
+    if (pipelinesTriggered.length === 0) {
+      throw new Error("No pipelines were triggered");
+    }
+
+    res.json({ pipelinesTriggered });
   } catch (error) {
     res.status(422).send(error.message);
   }
